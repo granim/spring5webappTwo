489ac3243dffb69b44f2fbfd40461623e11a7327
Grant Murphy <grantsmurphy@gmail.com>
2018-11-28 10:32:11 -0500

    Initial Commit

---
+++ src/main/java/com/example/spring5webappTwo/Spring5webappTwoApplication.java
@@ -0,0 +1,12 @@
+package com.example.spring5webappTwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5webappTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5webappTwoApplication.class, args);
+	}
+}
